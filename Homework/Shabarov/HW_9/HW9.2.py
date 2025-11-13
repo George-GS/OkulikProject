d90119b9e96da062502754c9e6cd3823785959a8
gs.shabarov <gs.shabarov@tensor.ru>
2025-11-13 22:51:49 +0300

    l10

--- Homework/Shabarov/HW_9/HW9.2.py
+++ Homework/Shabarov/HW_9/HW9.2.py
@@ -1,1 +1,23 @@
-2
+#Задание 2: Фильтрация пользователей
+# Есть список пользователей с возрастом
+users = [
+    {"name": "John", "age": 17},
+    {"name": "Anna", "age": 25},
+    {"name": "Mike", "age": 16},
+    {"name": "Sarah", "age": 30}
+]
+# Оставить только совершеннолетних (18+)
+# Используй filter
+
+
+# age_plus = []
+# def scan_age(age):
+#     for x in age:
+#         if x['age'] >= 18:
+#             age_plus.append(x)
+#     return age_plus
+
+adult_users = list(filter(lambda x: x['age'] >= 18, users))
+
+print(adult_users)
+
--- Homework/Shabarov/HW_9/HW9.3.py
+++ Homework/Shabarov/HW_9/HW9.3.py
@@ -1,1 +1,15 @@
-2
+#Задание 3: Генерация тестовых дат
+
+# Создай дату: 15 марта 2024 года
+# Преобразуй её в формат "15.03.2024"
+# Используй datetime
+
+import datetime
+
+gs_date = datetime.datetime(2024, 3, 15)
+print(gs_date)
+
+form_gs_date = gs_date.strftime('%d.%m.%Y')
+print(form_gs_date)
+
+print(gs_date.year)
